cb38da775b5624fe8534dffc657ca961ea6c9b7a
jwj019 <justin.johnson5@nih.gov>
2023-05-15 13:06:37 -0400

    Create issue from json response from graphql

--- .github/scripts/create_issues.py
+++ .github/scripts/create_issues.py
@@ -29,4 +29,21 @@ query = f"""
 """
 
 response = requests.post('https://api.github.com/graphql', headers=headers, json={'query': query})
-print(response.json())
+data = response.json()
+
+alerts = data["data"]["repository"]["vulnerabilityAlerts"]["nodes"]
+
+for alert in alerts:
+  package_name = alert["securityVulnerability"]["package"]["name"]
+  description = alert["securityVulnerability"]["advisory"]["description"]
+
+  # Check if an issue already exists
+  issue_exists = any(issue.title == package_name for issue in repo.get_issues(state="open"))
+
+  if not issue_exists:
+    # Create a new issue
+    repo.create_issue(
+      title=package_name,
+      body=description,
+      labels=["security"]
+    )
